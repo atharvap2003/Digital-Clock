488473ff3527444354397bec7cf2df6d7f08d12b
Atharva Pandharikar <atharvapandharikar5@gmail.com>
2022-11-08 10:18:13 +0530

    Updated

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function displayTime() {
     let dateTime = new Date();
     
+    
     let hrs = dateTime.getHours();
     let min = dateTime.getMinutes();
     let sec = dateTime.getSeconds();
+    datenum = dateTime.getDate();
+    month = dateTime.getUTCMonth();
+    year = dateTime.getFullYear();
     let session = document.getElementById('time');
     
+    const cars = ["Jan", "Feb", "March","April","May","June","July","Aug","Sept","Oct","Nov","Dec"];
     if(hrs>=12){
         hrs = hrs - 12;
         session.innerHTML = 'PM';
@@ -15,6 +20,9 @@ function displayTime() {
     }
     document.getElementById('hours').innerHTML = hrs;
     document.getElementById('minutes').innerHTML = min;
-    document.getElementById('seconds').innerHTML = sec; 
+    document.getElementById('seconds').innerHTML = sec;
+    document.getElementById('day').innerHTML = datenum; 
+    document.getElementById('month').innerHTML = month;
+    document.getElementById('year').innerHTML = year;
 }
 setInterval(displayTime,10);
